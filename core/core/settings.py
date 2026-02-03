74413281430c0b34638b75b159a0e0618706115c
suvinrj <79426136+suvinrj@users.noreply.github.com>
2026-02-03 15:11:41 +0800

    update the initialize django and LLMConfig database

--- core/core/settings.py
+++ core/core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i#b8hcho+g%9-5q89atgzp_ewpoyp7!d!&nlzrqfu!r8522cyx'
+SECRET_KEY = 'django-insecure-93v)+#e96ao7w43-et9oj%hg!st^eo$7n-ll7z=4(e$kq3l5f!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'llm_config',
 ]
 
 MIDDLEWARE = [
--- core/llm_config/models.py
+++ core/llm_config/models.py
@@ -1,3 +1,13 @@
+# llm_config/models.py
 from django.db import models
 
-# Create your models here.
+class LLMConfig(models.Model):
+    name = models.CharField(max_length=100)
+    prompt_template = models.TextField()
+    model_name = models.CharField(max_length=50)
+    temperature = models.FloatField(default=0.7)
+    language = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
